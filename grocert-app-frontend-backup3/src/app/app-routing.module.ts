6d70d70059059569a403661c6490b1fc60eee51c
Ranjitha KR <ranjithakr4747@gmail.com>
2021-05-21 12:10:55 +0530

    backup3

---
+++ grocert-app-frontend-backup3/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+
+const routes: Routes = [
+  {path: 'order-history', component: OrderHistoryComponent},
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'cart-details', component: CartDetailsComponent},
+  {path: 'products/:id', component: ProductDetailsComponent},
+  {path: 'search/:keyword', component: ProductListComponent},
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path:'contact',component:ContactUsComponent},
+  {path:'about', component:AboutUsComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ grocert-app-frontend-backup3/src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import {RouterModule,Routes} from '@angular/router';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {FontAwesomeModule} from '@fortawesome/fontawesome-free';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductCategoryComponent } from './components/product-category/product-category.component';
+import { SearchComponent } from './components/search/search.component';
+import {HttpClientModule} from '@angular/common/http';
+import { ProductService } from './services/product.service';
+import { CartService } from './services/cart.service';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { OrderHistoryComponent } from './components/order-history/order-history.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductCategoryComponent,
+    SearchComponent,
+    ProductDetailsComponent,
+    CartDetailsComponent,
+    CartStatusComponent,
+    CheckoutComponent,
+    OrderHistoryComponent,
+    ContactUsComponent,
+    AboutUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    NgbModule,
+    RouterModule
+  ],
+  providers: [ProductService,CartService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
